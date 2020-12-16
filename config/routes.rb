a5bb2177ef88f5cf63fb355ae13e047de1cf2370
mfaiza <mfaizaschool@gmail.com>
2020-12-16 15:40:39 +0700

    build layout and add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new', to: 'sessions#new'
+  root to: 'chatroom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
