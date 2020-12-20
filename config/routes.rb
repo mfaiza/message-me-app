ae923deac7959f1ac9ad21165c3bd74827e22aa5
mfaiza <mfaizaschool@gmail.com>
2020-12-20 09:49:22 +0700

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as: "login"
   root to: 'chatroom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
