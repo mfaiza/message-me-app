19d1191e60ae2d810eade09bc736f4ee388be373
mfaiza <mfaizaschool@gmail.com>
2020-12-21 23:57:47 +0700

    add message from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  post '/message', to: 'messages#create'
   root to: 'chatroom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
