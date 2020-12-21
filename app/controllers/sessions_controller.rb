8a2d1f668427b71dbd8bede9fd6fd2745288d28c
mfaiza <mfaizaschool@gmail.com>
2020-12-21 23:05:10 +0700

    restricting view access

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+	before_action :logged_in_redirect, :only => [:new, :create]
+
   def new
   end
 
@@ -18,6 +20,15 @@ class SessionsController < ApplicationController
   	session[:user_id] = nil
   	flash[:success] = "You has logged out!!"
   	redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+  	if logged_in?
+  		flash[:error] = "You are already logged in."
+  		redirect_to root_path
+  	end
   end
 
 end
