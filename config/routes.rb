d720ac41d8fa983794e08128c063d805af7a76fc
mfaiza <mfaizaschool@gmail.com>
2020-12-21 15:47:13 +0700

    add create and destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new', as: "login"
+  get 'login', to: 'sessions#new', as: "login"  
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root to: 'chatroom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
