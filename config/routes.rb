7b71a8e83d0923e0ab9abb9276fd25956e223c47
mfaiza <mfaizaschool@gmail.com>
2020-12-24 09:50:51 +0700

    set up ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   post '/message', to: 'messages#create'
   root to: 'chatroom#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  mount ActionCable.server, at: '/cable'
 end
